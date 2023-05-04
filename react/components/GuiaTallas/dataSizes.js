71061fdae3639b13356251457caf95ac2d2a0d21
David Leal Peláez <davidlealpelaez@gmail.com>
2023-05-03 19:16:34 -0500

    SE anexa el dropdown y la logica para filtrar el elemento deseado

--- react/components/GuiaTallas/dataSizes.js
+++ react/components/GuiaTallas/dataSizes.js
@@ -1,7 +1,3 @@
-import React, { useState } from "react"
-import { Dropdown } from 'vtex.styleguide'
-//import dataSizes from './dataSizes'
-
 const dataSizes = [
     {
         brand: "Adidas",
@@ -434,99 +430,46 @@ const dataSizes = [
         brand: "Ropa",
         person: [
             {
-                type: "Hombre",
+                type: "",
                 measures: [
-
                     {
                         title: "MEDIDA DE PIE (cm)",
-                        sizes: ["24", "24,5", "25", "26", "26,5", "27", "27,5", "28", "28,5", "29,5", "30"]
-                    },
-                    {
+                        sizes: [],
                         title: "COLOMBIA",
-                        sizes: ["36.5", "37", "37.5", "38", "39", "40", "41", "41,5", "42", "43,5", "44"]
-                    },
-                    {
+                        sizes: [],
                         title: "USA",
-                        sizes: ["07.0", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "12", "13.0"]
+                        sizes: []
                     }
-
                 ]
             },
             {
-                type: "Mujer",
+                type: "",
                 measures: [
-
                     {
                         title: "MEDIDA DE PIE (cm)",
-                        sizes: ["22", "22,5", "23", "23,5", "24", "24,5", "25", "25,5", "26", "26,5", "27"]
-                    },
-                    {
+                        sizes: [],
                         title: "COLOMBIA",
-                        sizes: ["34,5", "35", "36", "36,5", "37", "37,5", "38", "38,5", "39", "39,5", "40"]
-                    },
-                    {
+                        sizes: [],
                         title: "USA",
-                        sizes: ["5", "5,5", "6", "6,5", "7", "7,5", "8", "8,5", "9", "9,5", "10"]
+                        sizes: []
                     }
-
                 ]
             },
             {
-                type: "Junior",
+                type: "",
                 measures: [
                     {
                         title: "MEDIDA DE PIE (cm)",
-                        sizes: ["18.0", "18.5", "19", "19,5", "20", "21", "21,5", "22", "22,5", "23", "23,5"]
-                    },
-                    {
+                        sizes: [],
                         title: "COLOMBIA",
-                        sizes: ["28", "29", "30", "31", "32", "32,5", "33", "33,5", "34", "34,5", "35"]
-                    },
-                    {
+                        sizes: [],
                         title: "USA",
-                        sizes: ["12.0", "13.0", "1.0", "2.0", "3.0", "3,5", "4", "4,5", "5", "5,5", "6"]
+                        sizes: []
                     }
-
                 ]
             }
         ]
     }
 ]
 
-const GuideSize = () => {
-
-    const [brands, setBrands] = useState('')
-
-    const options = [
-        { value: 'adidas', label: 'Adidas' },
-        { value: 'nike', label: 'Nike' },
-        { value: 'reebok', label: 'Reebok' },
-        { value: 'fila', label: 'Fila' },
-        { value: 'lotto', label: 'Lotto' },
-        { value: 'ropa', label: 'Ropa' },
-    ]
-
-    const handleReferenceSizeBrand = (value:any) => {
-        
-        setBrands(value)
-        const result = dataSizes.filter( elem => elem.brand.toLowerCase() === value.toLowerCase())
-        console.log(result, "--->", value)
-    }
-
-    console.log(brands, "--->ESTADO")
-
-    return (
-        <div>
-            <Dropdown
-                label="Selecciona una marca"
-                size="large"
-                options={options}
-                value={brands}
-                //@ts-ignore
-                onChange={(_: any, value: any) => handleReferenceSizeBrand(value)}
-            />
-        </div>
-    )
-}
-
-export default GuideSize
+export default dataSizes
